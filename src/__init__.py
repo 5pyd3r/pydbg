5908ed9a17859da04caabf6268a56eafc17a2c95
5pyd3r <5pyd3r.tarsylia@gmail.com>
2022-10-03 22:10:45 +0800

    revert changes not related to github action

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-from pydbg.impl import Debugger
--- src/impl/__impl.pyx
+++ src/impl/__impl.pyx
@@ -34,12 +34,8 @@ cdef class Debugger:
         else:
             return False
 
-    def detach(self, process):
-        if DebugActiveProcessStop(process) != 0:
-            del self.process[process]
-            return True
-        else:
-            return False
+    def detach(self):
+        
 
     def run(self, timeout=INFINITE):
         cdef DEBUG_EVENT event
